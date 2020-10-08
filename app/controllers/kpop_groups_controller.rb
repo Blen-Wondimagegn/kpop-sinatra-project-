3d67fb6a476916e9216e5f559f82aa45d085d240
blen_meku <mekuriablen@gmail.com>
2020-10-08 03:50:07 -0700

    created CRUD

--- app/controllers/kpop_groups_controller.rb
+++ app/controllers/kpop_groups_controller.rb
@@ -1,35 +1,59 @@
 class KpopGroupsController < ApplicationController
-get '/groups' do  
+   get '/groups' do  
         @kpop_groups = KpopGroup.all 
         erb :"kpop_groups/index"
    end
-   get '/groups/new' do 
+
+    get '/groups/new' do 
       @users = User.all
       erb :"kpop_groups/new"
    end 
-
-    get '/groups/:id' do
+  
+   get '/kpop_groups/:id' do
         # redirect_if_not_logged_in
         @group = KpopGroup.find_by_id(params[:id])
         erb :"kpop_groups/show"
+        
     end
-   
+
     post '/groups' do
         group = KpopGroup.new(params)
         if group.save
             redirect "/kpop_groups/#{group.id}"
         else
             redirect "kpop_groups/new"
+
         end
+        
     end
-    get '/posts/:id/edit' do
-        redirect_if_not_logged_in
+
+    get '/kpop_groups/:id/edit' do
+        # redirect_if_not_logged_in
         @users = User.all
-        @post = Post.find_by_id(params[:id])
-        if @post.user.id == current_user.id
-            erb :"posts/edit"
+        @group = KpopGroup.find_by_id(params[:id])
+        # if @group.user.id == current_user.id
+            erb :"kpop_groups/edit"
+        # else
+        #     redirect "/groups"
+        # end
+    end
+
+     patch '/groups/:id' do
+        @group = KpopGroup.find_by_id(params[:id])
+        params.delete("_method")
+        if @group.update(params)
+            redirect "/kpop_groups/#{@group.id}"
         else
-            redirect "/posts"
+            redirect "kpop_groups/new"
         end
+         # binding.pry 
+      end
+
+    delete '/groups/:id' do
+        @group = KpopGroup.find_by_id(params[:id])
+        # if @group.user.id == current_user.id
+           @group.destroy
+        # end
+        redirect "/groups"
     end
 end   
