703a6cb52a5840aad07cc6675fd139fdc7e862cb
blen_meku <mekuriablen@gmail.com>
2020-10-11 18:45:38 -0700

    modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,36 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :sessions, true 
+    set :session_secret, "funsecret"
   end
 
   get "/" do
-    erb :welcome
+    erb:welcome 
+  end 
+
+  helpers do
+    def logged_in?
+      session[:user_id]
+    end
+
+    def current_user
+      @user ||= User.find_by(id: session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login"
+      end
+    end
+
+    def redirect_if_logged_in
+      if logged_in?
+        redirect "/groups"
+      end
+    end
+
   end
 
 end
